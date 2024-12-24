37afc8b3fb2d398c4135c06cce42786c346aa15d
srilathadinakar <srilathadinakar@gmail.com>
2024-12-24 15:54:37 +0530

    updated

--- src/Services/api.js
+++ src/Services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL:"http://localhost:5000/api"
-    baseURL:"https://online-assessment-backend-m1e6.onrender.com/",
+    baseURL:"https://online-assessment-backend-m1e6.onrender.com/api",
 })
 
 api.interceptors.request.use((config) => {
